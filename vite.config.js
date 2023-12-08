ef991cbb6591895a9972291705dce155cb243d92
Jhonatan Veliz <117142595+JhonatanVeliz@users.noreply.github.com>
2023-12-07 21:08:22 -0600

    env added

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env.VITE_API_URL': JSON.stringify('https://upconstthebackendykm.onrender.com/'),
+  },
 })
